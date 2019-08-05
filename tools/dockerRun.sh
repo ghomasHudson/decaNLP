45f80f1ce543f630c70004e2651b87272f87e6bc
Thomas Hudson <07thomas.hudson@gmail.com>
2019-08-05 14:18:31 +0100

    Docker file supports symlinks

--- tools/dockerRun.sh
+++ tools/dockerRun.sh
@@ -1,1 +1,15 @@
-docker run --runtime=nvidia -it --rm --memory 50g --memory-swap -1 -v "$(pwd):/decaNLP" -u $(id -u):$(id -g) bmccann/decanlp:cuda10_torch1latest bash
+# Starts decanlp docker environment
+# Docker doesn't support symbolic links
+# Fix by finding all links and mounting them as volumes
+volumes="-v $(pwd):/decaNLP "
+
+for f in *; do
+    if [ -d "$f" ]; then
+        if [ -d "$(readlink $f)" ]; then
+            echo "Mounting symbolic link: '$f' as volume"
+            volumes+="-v $(realpath $f):/decaNLP/$f "
+        fi
+    fi
+done
+docker run --runtime=nvidia -it --rm --memory 50g --memory-swap -1 $volumes -u $(id -u):$(id -g) bmccann/decanlp:cuda10_torch1latest bash
+
